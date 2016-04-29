06841580a077ab298a355624a1fd83f735eb4bfc
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-04-29 16:34:32 +0200

    dedicated SSD environment

---
+++ bin/custom/env-ssd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -z $PBS_JOBID ]; then
+  echo "No PBS environment detected."
+  exit 1
+fi
+
+export HOSTNAME=$(hostname)
+
+#export HADOOP_PREFIX="$WORK/hadoop-2.7.1"
+export HADOOP_PREFIX="$WORK/hadoop/hadoop-dist/target/hadoop-2.7.1"
+export HADOOP_HOME=$HADOOP_PREFIX
+export HADOOP_CONF_DIR="$HADOOP_PREFIX/conf/$PBS_JOBID"
+
+export SPARK_HOME="$WORK/spark-1.6.0-bin-without-hadoop"
+#export FLINK_HOME="$WORK/flink-0.10.2"
+export FLINK_HOME="$WORK/flink/build-target"
+
+export HIBENCH_HOME=$WORK/HiBench-ssd
+
+IFS=$'\n' read -d '' -r -a HADOOP_NODES < $PBS_NODEFILE
+export HADOOP_NODES
+export NUM_HADOOP_NODES=${#HADOOP_NODES[@]}
+if [ $NUM_HADOOP_NODES -lt 2 ]; then
+  echo "Please specify at least two nodes."
+  exit 1
+fi
+
+export HADOOP_NAMENODE=$(hostname) # ${HADOOP_NODES[0]}
+hadoop_namenode=($HADOOP_NAMENODE) # tmp array var to remove from data nodes array
+export HADOOP_DATANODES=(${HADOOP_NODES[@]/$hadoop_namenode})
+export NUM_HADOOP_DATANODES=${#HADOOP_DATANODES[@]}
+
+export NUM_GFS=2
+export SCRATCH=/flash/scratch5
+
+# node-local directory for HDFS
+export HDFS_LOCAL_DIR="$USER/hdfs/$PBS_JOBID"
+export HDFS_LOCAL_LOG_DIR="$HDFS_LOCAL_DIR/log"
