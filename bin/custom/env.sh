dd0ae3ac6c4c73c7616e405c59cab9915c2690e4
Robert Schmidtke <ro.schmidtke@gmail.com>
2016-05-02 21:16:38 +0200

    env for streaming

--- bin/custom/env.sh
+++ bin/custom/env.sh
@@ -18,18 +18,28 @@ export FLINK_HOME="$WORK2/flink/build-target"
 
 export HIBENCH_HOME=$WORK2/HiBench
 
-IFS=$'\n' read -d '' -r -a HADOOP_NODES < $PBS_NODEFILE
-export HADOOP_NODES
-export NUM_HADOOP_NODES=${#HADOOP_NODES[@]}
+export ZOOKEEPER_HOME=$WORK2/zookeeper-3.3.6
+export KAFKA_HOME=$WORK2/kafka_2.10-0.8.1
+
+IFS=$'\n' read -d '' -r -a nodes < $PBS_NODEFILE
+export NUM_KAFKA_NODES=${NUM_KAFKA_NODES:-0}
+export NUM_HADOOP_NODES=$((${#nodes[@]} - $NUM_KAFKA_NODES))
 if [ $NUM_HADOOP_NODES -lt 2 ]; then
   echo "Please specify at least two nodes."
   exit 1
 fi
 
+export HADOOP_NODES=(${nodes[@]:0:$NUM_HADOOP_NODES})
+
 export HADOOP_NAMENODE=$(hostname) # ${HADOOP_NODES[0]}
 hadoop_namenode=($HADOOP_NAMENODE) # tmp array var to remove from data nodes array
 export HADOOP_DATANODES=(${HADOOP_NODES[@]/$hadoop_namenode})
 export NUM_HADOOP_DATANODES=${#HADOOP_DATANODES[@]}
+
+export KAFKA_NODES=(${nodes[@]:$NUM_HADOOP_NODES:$NUM_KAFKA_NODES})
+
+export NUM_PRODUCER_NODES=${NUM_PRODUCER_NODES:-1}
+export PRODUCER_NODES=(${nodes[@]:0:$NUM_PRODUCER_NODES})
 
 export NUM_GFS=2
 export SCRATCH=/flash/scratch5
@@ -37,3 +47,14 @@ export SCRATCH=/flash/scratch5
 # node-local directory for HDFS
 export HDFS_LOCAL_DIR="$USER/hdfs/$PBS_JOBID"
 export HDFS_LOCAL_LOG_DIR="$HDFS_LOCAL_DIR/log"
+
+export KAFKA_DEFAULT_PARTITIONS=16
+export KAFKA_PORT=9092
+
+export ZOOKEEPER_PORT=2181
+export ZOOKEEPER_NODE=$HADOOP_NAMENODE
+
+export MAHOUT_HOME=$WORK2/apache-mahout-distribution-0.11.1
+
+# util functions
+function join_array { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
